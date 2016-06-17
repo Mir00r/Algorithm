1c66c489aad5bc65b64d013f2af0d3ffe8afa500
Abdur Razzak <abdur.razzaq06@gmail.com>
2016-06-17 15:40:11 +0600

    Depth first search algorithm implementation using object oriented style

---
+++ Graph_Algorithm/DFS/DFS_OOP.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define LI long int
+#define LLI long long int
+#define LL __int64
+#define ULL unsigned long long
+#define LLU long long unsigned
+#define row 5
+#define col 5
+#define MAX 100000 + 5
+#define jora pair <int, int>
+#define memo(array, value) memset(array, value, sizeof(array))
+#define pb push_back
+#define NL puts ("")
+#define inf (1 << 28)
+#define eps 1e9
+#define MOD 7477777
+#define PI 3.1415926535897932384626433832795
+#define PrimeRange 1000000
+#define CharRange 255
+
+class Graph{
+    private:
+        int vertices; // No of vertices
+        vector < int > *AdjMat; // Pointer to an array containing adjacency list
+    public:
+        Graph ( int v );    // Constructor
+        void AddEdge( int from, int to );    // function to add an edge to graph
+        void DFS ( int start );  // prints DFS traversal of the complete graph
+};
+
+Graph::Graph ( int v ) { // Constructor to set the number of vertices
+    this->vertices = v;
+    AdjMat = new vector < int >[ v ];
+}
+
+void Graph::AddEdge ( int from, int to ) { // function to add an edge to Graph
+    AdjMat[ from ].pb(to);
+}
+
+void Graph::DFS ( int start ){ // DFS Traverse of the vertices reachable from start
+    bool *visited = new bool[ this->vertices ]();
+    stack < int > st;
+
+    st.push ( start );
+    vector < int >::iterator it;
+
+    puts ("DFS Started: ");
+
+    while ( !st.empty() ) {
+        int top = st.top();
+        printf ("Stack Top value -> %d ", top );
+        st.pop();
+        NL;
+
+        // Mark the Current node as a visited
+        visited[ top ] = true;
+
+        // iterat for all the vertices in the adjacency list
+        for ( it = AdjMat[ top ].begin(); it != AdjMat[ top ].end(); it++ ) {
+            if ( !visited[ *it ] )
+                st.push( *it );
+        }
+    }
+    NL;
+}
+
+void Test_Input() {
+    // Creat a graph to perform DFS
+    Graph g( 4 );
+
+    g.AddEdge ( 0, 1 );
+    g.AddEdge ( 0, 2 );
+    g.AddEdge ( 1, 2 );
+    g.AddEdge ( 2, 0 );
+    g.AddEdge ( 2, 3 );
+    g.AddEdge ( 3, 3 );
+
+    printf ("Following is Depth First Traversal (startting from vertices 2) \n");
+    g.DFS ( 2 );
+}
+
+int main () {
+
+    //Test_Input();
+
+    int ver, edge, i, j, s, d;
+
+    puts ("Enter number of Vertices: ");
+    scanf ("%d", &ver);
+    Graph g( ver );
+    NL;
+
+    puts ("Enter number of Edges: ");
+    scanf ("%d", &edge);
+    for ( i = 0; i < edge; i++ ) {
+        puts ("Enter edges relation from and to: ");
+        scanf ("%d %d", &s, &d);
+        g.AddEdge ( s, d );
+    }
+    NL;
+
+    int sr;
+    puts ("Enter source where DFS is starting: ");
+    scanf ("%d", &sr);
+    g.DFS ( sr );
+
+    return 0;
+}
